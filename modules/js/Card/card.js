926e6bbc0efb149b46df957680c4c38f3d76f4ca
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2021-06-07 21:54:16 -0400

    padding change

--- modules/js/Card/card.js
+++ modules/js/Card/card.js
@@ -5,13 +5,13 @@ class Card extends Base {
         super();
         this.top = "5%"
         this.left = "5%"
-        this.padding = "2%"
+        this.padding = "2.5%"
         this.backgroundColor = "rgb(240, 238, 238)"
         this.borderRadius = "1.5%"
         this.display = "inline-block"
         this.position = "relative"
         this.textAlign = "center"
-        this.boxShadow = "2px 4px #868181"
+        this.boxShadow = "3px 5px #868181"
         this.margin = "1%"
     }
     connectedCallback() {
