e9ebffe399b1c3de1ec1f23833b6f072cb69e58f
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2021-06-07 21:50:45 -0400

    border radius change

--- modules/js/Card/card.js
+++ modules/js/Card/card.js
@@ -7,11 +7,11 @@ class Card extends Base {
         this.left = "5%"
         this.padding = "2%"
         this.backgroundColor = "rgb(240, 238, 238)"
-        this.borderRadius = "1%"
+        this.borderRadius = "1.5%"
         this.display = "inline-block"
         this.position = "relative"
         this.textAlign = "center"
-        this.boxShadow = "0px 4px #868181"
+        this.boxShadow = "2px 4px #868181"
         this.margin = "1%"
     }
     connectedCallback() {
