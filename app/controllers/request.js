d225d0b449f3bd8130334ad0b22c079a5730d875
justdancepc <jesusdavid268@outlook.com>
2021-07-21 02:10:45 -0400

    reflection class

--- app/controllers/request.js
+++ app/controllers/request.js
@@ -1,25 +1,29 @@
+class Methods {
+  sayHello() {
+    console.log("Hello there!")
+    return "Hello there!"
+  }
+
+  sayBye() {
+    console.log("Bye!")
+    return "Bye!"
+  }
+}
+
+const methods = new Methods()
+
 const receiveRequest = (req, res) => {
   const {method} = req.body
   if (!method) {
     return res.status(400).json({msg: "Method not specified."})
   }
   try {
-    const msg = this[method]()
+    const msg = methods[method]()
     res.status(200).json({msg})
   } catch (e) {
     console.error(e)
     res.status(404).json({msg: "Method not found."})
   }
-}
-
-function sayHello() {
-  console.log("Hello there!")
-  return "Hello there!"
-}
-
-function sayBye() {
-  console.log("Bye!")
-  return "Bye!"
 }
 
 module.exports = {
